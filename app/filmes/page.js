48fdd246fcd367f06af6fb1704f590d26317d139
João Vitor Cangussu <jvcangussubo@gmail.com>
2025-04-29 19:52:33 -0300

    Header e menu de links criados

--- app/filmes/page.js
+++ app/filmes/page.js
@@ -1,5 +1,19 @@
+import MainHeader from "@/components/main-header/main-header";
+import styles from "./page.module.css";
+import Link from "next/link";
+
 export default function FilmesPage() {
   return (
-    <p>""</p>
+    <>
+      <MainHeader />
+      <div className={styles.linksContainer}>
+        <Link href="/filmes/em-alta" className={styles.link}>
+          <p>Apenas filmes em alta</p>
+        </Link>
+        <Link href="/filmes/filmes-por-ano" className={styles.link}>
+          <p>Filmes por ano de lançamento</p>
+        </Link>
+      </div>
+    </>
   );
 }
